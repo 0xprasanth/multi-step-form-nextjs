092768b556620663480eb09e8d300f52ad96d607
0xprasanth <raviprasanth45@gmail.com>
2024-12-26 11:30:00 +0530

    feat: implement multi-step form with navigation and completion handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,66 @@
+"use client";
+import { FormProvider } from "@/hook/use-form-context";
+import StepOneForm from "@/modules/forms/step1";
+import StepTwoForm from "@/modules/forms/step2";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [step, setStep] = useState(1);
+  const [isComplete, setIsComplete] = useState(false);
+  const handleNext = () => {
+    setStep(step + 1);
+  };
+
+  const handlePrev = () => {
+    setStep(step - 1);
+  };
+
+  const handleComplete = () => {
+    setIsComplete(true);
+  };
+  // render each step based on the current step state
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <StepOneForm onNext={handleNext} />;
+      case 2:
+        return <StepTwoForm onNext={handleComplete} onPrev={handlePrev} />;
+      default:
+        null;
+    }
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Hello Nextjs app with Tailwind CSS
+    <FormProvider>
+      <main className="min-h-screen p-8">
+        <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md text-black border ">
+          {!isComplete ? (
+            <div className="mb-4">
+              <h1 className="text-2xl font-bold text-center mb-4">
+                Multi-Step Form
+              </h1>
+              <div className="flex justify-center mb-4">
+                <div className="flex mb-4">
+                  <div
+                    className={`w-1/2 h-2 ${
+                      step >= 1 ? "bg-blue-500" : "bg-gray-200"
+                    }`}
+                  ></div>
+                  <div
+                    className={`w-1/2 h-2 ${
+                      step >= 2 ? "bg-blue-500" : "bg-gray-200"
+                    }`}
+                  ></div>
+                </div>
+                <p className="text-md text-gray-500">Step {step} of 2</p>
+              </div>
+              {renderStep()}
+            </div>
+          ) : (
+            <>Form Completed</>
+          )}
+        </div>
       </main>
-    </div>
+    </FormProvider>
   );
 }
